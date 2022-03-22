802029aee0bafa4310ff431f8caf40958d5a5251
Transcription Factory <transcriptionfactory@gmail.com>
2022-03-22 12:21:47 -0700

    Update movingBox.js

--- docs/assets/movingBox.js
+++ docs/assets/movingBox.js
@@ -1,6 +1,9 @@
+<head>
+    <base href="../jstest.md">
+
 var id = null;
-function myMove(docID) {
-  var elem = docID.getElementById("myAnimation");   
+function myMove() {
+  var elem = document.getElementById("myAnimation");   
   var pos = 0;
   clearInterval(id);
   id = setInterval(frame, 10);
@@ -14,3 +17,4 @@ function myMove(docID) {
     }
   }
 }
+</head>
